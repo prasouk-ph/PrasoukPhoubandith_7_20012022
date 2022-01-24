5b146a4f035e397a4eb3590cc03222bd156191e5
prasouk-ph <prasouk.ph@gmail.com>
2022-01-24 15:28:35 +0100

    Display recipes cards

---
+++ scripts/factories/recipe.js
@@ -0,0 +1,121 @@
+class RecipeFactory {
+    constructor(recipe) {
+        this._id = recipe.id;
+        this._name = recipe.name;
+        this._servings = recipe.servings;
+        this._ingredients = recipe.ingredients;
+        this._time = recipe.time;
+        this._description = recipe.description;
+        this._appliance = recipe.appliance;
+        this._ustensils = recipe.ustensils;
+    }
+
+    getId() {
+        return this._id;
+    }
+
+    getName() {
+        return this._name;
+    }
+
+    getServings() {
+        return this._servings;
+    }
+
+    getIngredients() {
+        return this._ingredients;
+    }
+
+    getTime() {
+        return this._time;
+    }
+
+    getDescription() {
+        return this._description;
+    }
+
+    getAppliance() {
+        return this._appliance;
+    }
+
+    getUstensils() {
+        return this._ustensils;
+    }
+
+    createCard() {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const cardPicture = document.createElement("div");
+        cardPicture.classList.add("card-picture");
+        const recipePicture = document.createElement("img");
+        recipePicture.classList.add("recipe-picture");
+
+        const cardContent = document.createElement("div");
+        cardContent.classList.add("card-content");
+        const contentHeader = document.createElement("div");
+        contentHeader.classList.add("content-header");
+        const recipeTitle = document.createElement("h2");
+        recipeTitle.classList.add("recipe-title");
+        recipeTitle.textContent = this._name;
+        const recipeDuration = document.createElement("p");
+        recipeDuration.classList.add("recipe-duration");
+        recipeDuration.textContent = `${this._time} min`;
+
+        const contentDetails = document.createElement("div");
+        contentDetails.classList.add("content-details");
+        const recipeIngredientsContainer = document.createElement("div");
+        recipeIngredientsContainer.classList.add("recipe-ingredients");
+
+        const recipeIngredients = this._ingredients;
+        recipeIngredients.forEach(ingredient => {
+            const ingredientModel = new IngredientFactory(ingredient);
+            const ingredientDOM = ingredientModel.createIngredient()
+            recipeIngredientsContainer.append(ingredientDOM);
+        });
+        
+        
+        const recipeNotice = document.createElement("p");
+        recipeNotice.classList.add("recipe-notice");
+        recipeNotice.textContent = this._description;
+
+
+        cardPicture.append(recipePicture);
+        contentHeader.append(recipeTitle, recipeDuration);
+        cardContent.append(contentHeader, contentDetails);
+        contentDetails.append(recipeIngredientsContainer, recipeNotice);
+        card.append(cardPicture, cardContent);
+        return card;
+    }
+}
+
+class IngredientFactory {
+    constructor(data) {
+        this.ingredient = data.ingredient;
+        this.quantity = data.quantity;
+        this.unit = data.unit;
+    }
+
+    getQuantity() {
+        if (this.quantity != undefined && this.unit != undefined) {
+            return `${this.quantity} ${this.unit}`;
+        } else if (this.quantity != undefined && this.unit == undefined) {
+            return this.quantity;
+        } else if (this.quantity == undefined && this.unit != undefined) {
+            return this.unit;
+        } else if (this.quantity == undefined && this.unit == undefined) {
+            return this.ingredient;
+        }
+    }
+
+    createIngredient() {
+        const ingredientsItem = document.createElement("div");
+        ingredientsItem.classList.add("ingredients-item");
+        const ingredientTitle = document.createElement("h3");
+        ingredientTitle.textContent = this.ingredient;
+        const ingredientQuantity = document.createElement("p");
+        ingredientQuantity.textContent = this.getQuantity();
+        ingredientsItem.append(ingredientTitle, ingredientQuantity);
+        return (ingredientsItem);
+    }
+}
---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+function init() {
+    const main = document.querySelector("main")
+    const recipesSection = document.createElement("div");
+    recipesSection.classList.add("section-recipes");
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("card-container");
+    recipesSection.append(cardContainer);
+    main.append(recipesSection);
+
+    recipes.forEach(recipe => {
+        const recipeModel = new RecipeFactory(recipe);
+        const recipeCard = recipeModel.createCard();
+        cardContainer.appendChild(recipeCard);
+    });
+}
+
+init();
