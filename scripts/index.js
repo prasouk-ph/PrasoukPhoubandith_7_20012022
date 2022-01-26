5c670f94e9ab22a047bda2e24113204e8847745b
prasouk-ph <prasouk.ph@gmail.com>
2022-01-26 23:36:19 +0100

    Add unclosable dropdown menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 function init() {
-    const main = document.querySelector("main")
+    const main = document.querySelector("main");
     const recipesSection = document.createElement("div");
     recipesSection.classList.add("section-recipes");
     const cardContainer = document.createElement("div");
@@ -15,3 +15,26 @@ function init() {
 }
 
 init();
+
+const menuIngredient = document.querySelector(".filter-menu-ingredient");
+menuIngredient.addEventListener("click", openDropdown);
+
+function openDropdown() {
+    const menuIngredientDropdownContent = document.querySelector(".filter-menu-content");
+    const menuIngredientGap = document.querySelector(".filter-menu-ingredient");
+    const ingredientOptions = document.querySelectorAll(".option-item");
+    
+    menuIngredientDropdownContent.style.display = "block";
+
+    if (ingredientOptions.length == 1) {
+        menuIngredientDropdownContent.style.width = "180px";
+        menuIngredientGap.style.marginRight = "50px";
+    } else if (ingredientOptions.length == 2) {
+        menuIngredientDropdownContent.style.width = "450px";
+        menuIngredientGap.style.marginRight = "320px";
+    } else if (ingredientOptions.length >= 3) {
+        menuIngredientDropdownContent.style.width = "680px";
+        menuIngredientGap.style.marginRight = "550px";
+    }
+    menuIngredientDropdownContent.classList.add("active")
+}
