9d22949fc1dcda3ff9de00a6c9b628f45c519546
prasouk-ph <prasouk.ph@gmail.com>
2022-01-31 19:00:42 +0100

    Refactoring with class

--- scripts/factories/filterItem.js
+++ scripts/factories/filterItem.js
@@ -1,73 +1,163 @@
-class IngredientFilterFactory {
+class TagMenu {
+    constructor(menuTitle, menuColor, tagType, recipes) {
+        this.menuTitle = menuTitle;
+        this.menuColor = menuColor;
+        this.tagType = tagType;
+        this.recipes = recipes;
+    }
+    
+
+    getElementDOM() {
+        // create tag elements
+        const menu = document.createElement("div");
+        menu.id = `menu-${this.tagType}`;
+        menu.classList.add("filter-menu", `filter-menu-${this.menuColor}`);
+
+        const menuTitle = document.createElement("div");
+        menuTitle.classList.add("filter-menu-title");
+        menuTitle.textContent = this.menuTitle;
+
+        const menuContent = document.createElement("div");
+        menuContent.classList.add("filter-menu-content", "filter-menu", `filter-menu-${this.menuColor}`);
+
+        const searchBar = document.createElement("input");
+        searchBar.id = `${this.tagType}-search-bar`;
+        searchBar.classList.add("tag-search-bar");
+        searchBar.setAttribute("type", "search");
+        searchBar.setAttribute("name", "filter-search");
+        searchBar.setAttribute("placeholder", `Rechercher un ${this.tagType}`);
+        
+
+        // get tag options
+        const tags = [];
+
+        this.recipes.forEach(recipe => {
+            if (!Array.isArray(recipe[`${this.tagType}`])) {
+                tags.push(recipe[`${this.tagType}`]);
+            }
+            else {
+                recipe[`${this.tagType}`].forEach(tagOption => {
+                    if (typeof tagOption === "object") {
+                        tags.push(tagOption.ingredient)
+                    } else {
+                        tags.push(tagOption)
+                    }
+                });
+            }
+        });
+
+        const tagsWithoutDuplicate = Array.from(new Set(tags));
+
+        const OptionsContainer = document.createElement("ul");
+        OptionsContainer.classList.add("filter-menu-options");
+        
+        const tagContainer = document.createElement("div");
+        tagContainer.classList.add("tag-container");
+        
+        tagsWithoutDuplicate.forEach(tagOption => {
+            const tagModel = new TagFactory(tagOption);
+            const tagDOM = tagModel.getElementDOM();
+            OptionsContainer.append(tagDOM);
+            tagDOM.addEventListener("click", addTag);
+            }
+        )
+
+
+        // append elements
+        menu.append(menuTitle, menuContent);
+        menuContent.append(searchBar, OptionsContainer);
+
+        menu.addEventListener("click", openDropdown);
+
+
+        // functions
+        function addTag(event) {
+            const tagContainer = document.querySelector(".tag-container");
+            const optionsContainer = event.target.parentNode;
+            const menuSelected = optionsContainer.parentNode.parentNode;
+            const menuDropdownSelected = optionsContainer.parentNode;
+            const tagOption = event.target;
+    
+            const tagButton = document.createElement("button");
+            tagButton.classList.add("button-tag");
+    
+            if (optionsContainer.parentNode.className.includes("green")) {
+                tagButton.classList.add("button-tag-green");
+            }
+            else if (optionsContainer.parentNode.className.includes("red")) {
+                tag.classList.add("button-tag-red");
+            }
+            else if (optionsContainer.parentNode.className.includes("blue")) {
+                    tagButton.classList.add("button-tag-blue");
+            }
+    
+            tagButton.textContent = tagOption.textContent;
+            tagContainer.append(tagButton);
+            tagOption.classList.add("hide");
+            tagButton.addEventListener("click", removeTag);
+    
+    
+            const optionsVisible = optionsContainer.querySelectorAll(":not(.hide)");
+            if (optionsVisible.length <= 1) {
+                menuDropdownSelected.style.width = "180px";
+                menuSelected.style.marginRight = "50px";
+            } else if (optionsVisible.length == 2) {
+                menuDropdownSelected.style.width = "450px";
+                menuSelected.style.marginRight = "320px";
+            } else if (optionsVisible.length >= 3) {
+                menuDropdownSelected.style.width = "680px";
+                menuSelected.style.marginRight = "550px";
+            }
+    
+
+            function removeTag() {
+                const tagContainer = document.querySelector(".tag-container");
+                
+                tagContainer.removeChild(tagButton);
+                tagOption.classList.remove("hide");
+            }
+        }
+
+
+        function openDropdown() {
+            const menuOptionsQty = OptionsContainer.querySelectorAll(":not(.hide)");
+            
+            menuContent.style.display = "block";
+
+            if (menuOptionsQty.length <= 1) {
+                menuContent.style.width = "180px";
+                menu.style.marginRight = "50px";
+            } else if (menuOptionsQty.length == 2) {
+                menuContent.style.width = "450px";
+                menu.style.marginRight = "320px";
+            } else if (menuOptionsQty.length >= 3) {
+                menuContent.style.width = "680px";
+                menu.style.marginRight = "550px";
+            }        
+
+            document.addEventListener("click", closeDropdown);
+
+
+            function closeDropdown(event) {
+                if (!menu.contains(event.target)) { // if target is not a child of the menu selected
+                    menuContent.style.display = "none";
+                    menu.style.marginRight = "0px";
+                    document.removeEventListener("click", closeDropdown);
+                } 
+            }
+        }
+
+        return menu;
+    }    
+}
+
+
+class TagFactory {
     constructor(ingredient) {
         this.name = ingredient;
     }
 
-    getName() {
-        return this.name;
-    }
-
-    createElementDOM() {
-        // const options = document.createElement("ul");
-        // options.classList.add("filter-menu-options");
-        // const li = document.createElement("li");
-        // li.classList.add("option-item");
-        // li.textContent = this.name;
-        // options.append(li);
-        // return options;
-
-        const li = document.createElement("li");
-        li.classList.add("option-item");
-        li.textContent = this.name;
-        return li;
-    }
-}
-
-
-class UstensilFilterFactory {
-    constructor(ustensil) {
-        this.name = ustensil;
-    }
-
-    getName() {
-        return this.name;
-    }
-
-    createElementDOM() {
-        // const options = document.createElement("ul");
-        // options.classList.add("filter-menu-options");
-        // const li = document.createElement("li");
-        // li.classList.add("option-item");
-        // li.textContent = this.name;
-        // options.append(li);
-        // return options;
-
-        const li = document.createElement("li");
-        li.classList.add("option-item");
-        li.textContent = this.name;
-        return li;
-    }
-}
-
-
-class ApplianceFilterFactory {
-    constructor(appliance) {
-        this.name = appliance;
-    }
-
-    getName() {
-        return this.name;
-    }
-
-    createElementDOM() {
-        // const options = document.createElement("ul");
-        // options.classList.add("filter-menu-options");
-        // const li = document.createElement("li");
-        // li.classList.add("option-item");
-        // li.textContent = this.name;
-        // options.append(li);
-        // return options;
-
+    getElementDOM() {
         const li = document.createElement("li");
         li.classList.add("option-item");
         li.textContent = this.name;
--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,25 @@
 const recipesDisplayed = recipes;
 
 function init() {
-    displayRecipe();
-    loadFilterItems();
-
-    const filterMenus = document.querySelectorAll(".filter-menu");
-    filterMenus.forEach(menu => menu.addEventListener("click", openDropdown));
-
-    const tagSearchBar = document.querySelectorAll(".tag-search-bar");
-    tagSearchBar.forEach(searchBar => searchBar.addEventListener("input", filterTags));
+    getTagMenus();
+    displayRecipes(recipesDisplayed);
 }
 
 init();
 
 
 // Functions
-function displayRecipe() {
+function displayRecipes(recipes) {
     const main = document.querySelector("main");
-    const recipesSection = document.createElement("div");
-    recipesSection.classList.add("section-recipes");
+
+    const sectionRecipes = document.createElement("div");
+    sectionRecipes.classList.add("section-recipes");
+
     const cardContainer = document.createElement("div");
     cardContainer.classList.add("card-container");
-    recipesSection.append(cardContainer);
-    main.append(recipesSection);
+
+    sectionRecipes.append(cardContainer);
+    main.append(sectionRecipes);
 
     recipes.forEach(recipe => {
         const recipeModel = new RecipeFactory(recipe);
@@ -31,168 +28,31 @@ function displayRecipe() {
     });
 }
 
-function loadFilterItems() {
-    // create array from recipe in card container, with name slice recipes variable, keep only the recipe contains in the first array created
-    // or create a copy of recipes variable  and slice it according to recipe in card container, use recipe id, get values from new array
-    // const recipesDisplayed = Array.from(document.querySelectorAll(".card"));
-    // console.log(recipesDisplayed[0])
+
+function getTagMenus() {
+    const main = document.querySelector("main");
+
+    const sectionFilter =  document.createElement("section");
+    sectionFilter.classList.add("section-filter");
+
+    const tagContainer =  document.createElement("div");
+    tagContainer.classList.add("tag-container");
+
+    const filterContainer =  document.createElement("div");
+    filterContainer.classList.add("filter-container");
+
+    const menuIngredients = new TagMenu("Ingrédients", "blue", "ingredients", recipesDisplayed);
+    const menuIngredientsDOM = menuIngredients.getElementDOM();
     
-    // for every recipes, get values from key appliance/utensils/ingredients
-    // const recipesDisplayed = recipes;
+    const menuAppliances = new TagMenu("Appareils", "green", "appliance", recipesDisplayed);
+    const menuAppliancesDOM = menuAppliances.getElementDOM();
     
+    const menuUstensils = new TagMenu("Ustensiles", "red", "ustensils", recipesDisplayed);
+    const menuUstensilsDOM = menuUstensils.getElementDOM();
     
-    // MENU APPLIANCE
-    const menuAppliance = document.querySelector("#menu-appliance");
-    const menuApplianceContainer = menuAppliance.querySelector(".filter-menu-content");
-
-    const applianceWithoutDuplicate = [];
-    recipesDisplayed.forEach(recipe => {
-        applianceWithoutDuplicate.push(recipe.appliance)
-    });
-    const appliancesWithoutDuplicate = Array.from(new Set(applianceWithoutDuplicate));
-    const applianceFilterOptionsContainer = document.createElement("div");
-    applianceFilterOptionsContainer.classList.add("filter-menu-options");
-    appliancesWithoutDuplicate.forEach(appliance => {
-        const applianceModel = new ApplianceFilterFactory(appliance);
-        const applianceDOM = applianceModel.createElementDOM();
-        applianceFilterOptionsContainer.append(applianceDOM);
-        }
-    )
-    menuApplianceContainer.append(applianceFilterOptionsContainer);
-
-
-    // MENU USTENSIL
-    const menuUstensil = document.querySelector("#menu-ustensils");
-    const menuUstensilContainer = menuUstensil.querySelector(".filter-menu-content");
-
-    const ustensils = [];
-    recipesDisplayed.forEach(recipe => {
-        recipe.ustensils.forEach(ustensil => {
-            ustensils.push(ustensil)
-        })
-    });
-    const ustensilsWithoutDuplicate = Array.from(new Set(ustensils));
-    const ustensilsFilterOptionsContainer = document.createElement("div");
-    ustensilsFilterOptionsContainer.classList.add("filter-menu-options");
-    ustensilsWithoutDuplicate.forEach(ustensil => {
-        const ustensilModel = new UstensilFilterFactory(ustensil);
-        const ustensilDOM = ustensilModel.createElementDOM();
-        ustensilsFilterOptionsContainer.append(ustensilDOM);
-        }
-    )
-    menuUstensilContainer.append(ustensilsFilterOptionsContainer);
-
-
-    // MENU INGREDIENT
-    const menuIngredient = document.querySelector("#menu-ingredients");
-    const menuIngredientContainer = menuIngredient.querySelector(".filter-menu-content");
-
-
-    const ingredients = [];
-    recipesDisplayed.forEach(recipe => {
-        recipe.ingredients.forEach(ingredient => {
-            ingredients.push(ingredient.ingredient)
-        })
-    });
-    const ingredientsWithoutDuplicate = Array.from(new Set(ingredients));
-    const ingredientsFilterOptionsContainer = document.createElement("div");
-    ingredientsFilterOptionsContainer.classList.add("filter-menu-options");
-    ingredientsWithoutDuplicate.forEach(ingredient => {
-        const ingredientModel = new IngredientFilterFactory(ingredient);
-        const ingredientDOM = ingredientModel.createElementDOM();
-        ingredientsFilterOptionsContainer.append(ingredientDOM);
-        }
-    )
-    menuIngredientContainer.append(ingredientsFilterOptionsContainer);
-
-
-    // Tag interaction
-    const optionItems = document.querySelectorAll(".option-item");
-    optionItems.forEach(item => item.addEventListener("click", addTag));
-
-    function addTag(event) {
-        const tagContainer = document.querySelector(".tag-container");
-        const optionsContainer = event.target.parentNode;
-        const menuSelected = optionsContainer.parentNode.parentNode;
-        const menuDropdownSelected = optionsContainer.parentNode;
-        const addedTag = event.target;
-
-        const tag = document.createElement("button");
-        tag.classList.add("button-tag");
-
-        if (optionsContainer.parentNode.className.includes("green")) {
-            tag.classList.add("button-tag-green");
-        }
-        else if (optionsContainer.parentNode.className.includes("red")) {
-            tag.classList.add("button-tag-red");
-        }
-        else if (optionsContainer.parentNode.className.includes("blue")) {
-                tag.classList.add("button-tag-blue");
-        }
-
-        tag.textContent = event.target.textContent;
-        tagContainer.append(tag);
-        addedTag.classList.add("hide");
-        tag.addEventListener("click", removeTag);
-
-
-        const optionsVisible = optionsContainer.querySelectorAll(":not(.hide)");
-        if (optionsVisible.length <= 1) {
-            menuDropdownSelected.style.width = "180px";
-            menuSelected.style.marginRight = "50px";
-        } else if (optionsVisible.length == 2) {
-            menuDropdownSelected.style.width = "450px";
-            menuSelected.style.marginRight = "320px";
-        } else if (optionsVisible.length >= 3) {
-            menuDropdownSelected.style.width = "680px";
-            menuSelected.style.marginRight = "550px";
-        }
-
-
-        function removeTag(event) {
-            const tagToremove = event.target;
-
-            tagContainer.removeChild(tagToremove);
-            addedTag.classList.remove("hide");
-        }
-    }
-}
-
-
-
-
-
-function openDropdown(event) {
-    const menuSelected = event.target.parentNode;
-    
-    if (menuSelected.id.includes("menu")) {
-        const menuDropdownSelected =  menuSelected.querySelector(".filter-menu-content");
-        const menuOptionsContainerSelected =  menuSelected.querySelector(".filter-menu-options");
-        const menuOptionsQty = menuOptionsContainerSelected.querySelectorAll(":not(.hide)");
-        
-        menuDropdownSelected.style.display = "block";
-
-        if (menuOptionsQty.length <= 1) {
-            menuDropdownSelected.style.width = "180px";
-            menuSelected.style.marginRight = "50px";
-        } else if (menuOptionsQty.length == 2) {
-            menuDropdownSelected.style.width = "450px";
-            menuSelected.style.marginRight = "320px";
-        } else if (menuOptionsQty.length >= 3) {
-            menuDropdownSelected.style.width = "680px";
-            menuSelected.style.marginRight = "550px";
-        }
-
-        document.addEventListener("click", closeModal);
-
-        function closeModal(event) {
-            if (!menuSelected.contains(event.target)) { // if target is not a child of the menu selected
-                menuDropdownSelected.style.display = "none";
-                menuSelected.style.marginRight = "0px";
-                document.removeEventListener("click", closeModal);
-            } 
-        }
-    }   
+    filterContainer.append(menuIngredientsDOM, menuAppliancesDOM, menuUstensilsDOM);
+    sectionFilter.append(tagContainer, filterContainer);
+    main.append(sectionFilter);
 }
 
 
@@ -247,3 +107,6 @@ function filterTags(event) {
         menuSelected.style.marginRight = "550px";
     }
 }
+
+
+
